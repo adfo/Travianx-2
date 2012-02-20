f3b6f62f5c00293d9692ecdc5110bfd5e2bcbbb8
GameEngine <paul.navalassie@gmail.com>
2012-02-20 10:14:26 +0100

    Update GameEngine/view/layout/footer.php

--- GameEngine/view/layout/footer.php
+++ GameEngine/view/layout/footer.php
@@ -3,6 +3,6 @@
 	<li><a href="index.php"><?php echo LANGUI_FOOTER_MAIN;?></a></li>
 	<li><a href="manual.php"><?php echo LANGUI_FOOTER_MANUAL;?></a></li>
 	<li><a href="training.php"><?php echo LANGUI_FOOTER_TRAINING;?></a></li>
-	<li><a href="http://loadingz.com" target="_blank">Copyright ©: </a></li>
-	<li><font color="red" size="2"><b><a href="mailto:admin@loadingz.com"><font color="red">admin@loadingz.com</font></a></b></font></li>
+	<li><a href="#" target="_blank">Copyright ©: </a></li>
+	<li><font color="red" size="2"><b><a href="mailto:#"><font color="red">admin</font></a></b></font></li>
 </ul>
